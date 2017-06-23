6096ace413736a33bf3983d87b8d140ac2cab36c
Matthew Menna <mattmenna@gmail.com>
2017-06-23 12:42:14 -0400

    Initial Commit

---
+++ HelloWorldCs/Hello.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+ * Taken from https://docs.microsoft.com/en-us/dotnet/csharp/programming-guide/inside-a-program/hello-world-your-first-program
+ * 
+ */
+
+namespace HelloWorldCs
+{
+    class Hello
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            //Keep the console window open in debug mode.
+            Console.WriteLine("Press any Key to Exit.");
+            Console.ReadKey();
+        }
+    }
+}
